87d1cbe77d5ecf0b84c42d8e2811a049b6646228
Administrator <paolocesar2004@gmail.com>
2015-03-06 17:01:42 -0400

    ultimo2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-alert(eroor|*)
+//this is a comment
+
+/*
+a multiple lines comment
+ */
+
+console.log('This is a code from main.js');
+
+name = window.prompt('Your name?')
+
+var getData = function ()
+{
+
+    var name = window.prompt('Your Name?');
+    var age = window.prompt('Your Age?');
+
+    if (age > 30)
+        console.log('You are an old person');
+    else
+        console.log('You are lucky!..still young.');
+
+};
+
+
